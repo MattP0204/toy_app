3b03b0ef51b5331d777a7ed0cc8c3f4b35e33912
Mel K <mproctor0204@gmail.com>
2014-11-28 15:03:42 -0500

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def home
-  	render text: "This is home."
+  	render text: "This is away."
   end
   	
 end
